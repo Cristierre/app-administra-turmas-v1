cf2a4da6cd9d8f943abd1561459b99b252fee39e
Cristierre Gomes Konrath <cristierre.konrath@dbserver.com.br>
2020-04-21 00:36:08 -0300

    Implementa intent e tela de cadastro

---
+++ app/src/main/java/com/example/app_administra_turmas_v1/Cadastro.java
@@ -0,0 +1,15 @@
+package com.example.app_administra_turmas_v1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+        
+    }
+}
